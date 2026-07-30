bf3644f70abe73854e340b6895880510df4248c9
kovalchukkris710-glitch <kovalchukkris710@gmail.com>
2026-07-30 18:13:55 +0300

    Create revenue_metrics_by_continent.sql

---
+++ sql/revenue_metrics_by_continent.sql
@@ -0,0 +1,70 @@
+-- Calculate total revenue and revenue by device type
+WITH revenue_usd AS (
+SELECT
+    sp.continent AS continent,
+    SUM(p.price) AS revenue,
+    SUM(case when sp.device = 'mobile' THEN p.price ELSE 0 END) AS  revenue_from_mobile,
+    SUM(case when sp.device = 'desktop' THEN p.price ELSE 0 END) AS  revenue_from_desktop
+FROM `DA.order` o
+JOIN `DA.product` p 
+  ON o.item_id=p.item_id
+JOIN `DA.session_params` sp 
+  ON o.ga_session_id=sp.ga_session_id
+GROUP BY sp.continent),
+
+-- Calculate unique sessions and accounts by continent
+registrations AS (
+SELECT
+    sp.continent AS continent,
+    COUNT(DISTINCT sp.ga_session_id) AS session_cnt,
+    COUNT(DISTINCT acs.account_id) AS account_cnt
+FROM `DA.session_params` sp
+LEFT JOIN `DA.account_session` acs
+  ON sp.ga_session_id=acs.ga_session_id
+GROUP BY sp.continent
+),
+
+-- Calculate the number of unique verified accounts by continent
+verified AS(
+SELECT
+    sp.continent,
+    COUNT(DISTINCT a.id) AS verified_account
+FROM `DA.account` a
+JOIN `DA.account_session` acs 
+  ON a.id =acs.account_id
+JOIN `DA.session_params` sp 
+  ON acs.ga_session_id = sp.ga_session_id
+WHERE a.is_verified = 1
+GROUP BY  sp.continent
+)
+
+-- Combine revenue, account and session metrics
+SELECT
+    registrations.continent,
+    revenue_usd.revenue,
+    revenue_usd.revenue_from_mobile,
+    revenue_usd.revenue_from_desktop,
+    -- Calculate each continent's share of total revenue
+    ROUND(
+      SAFE_DIVIDE(
+      revenue_usd.revenue,
+      (
+      SELECT SUM(p.price)
+      FROM `DA.order` AS o
+      JOIN `DA.product` AS p
+        ON o.item_id = p.item_id
+      )
+      ) * 100,
+      2
+    ) AS percent_revenue_from_total,
+    registrations.account_cnt,
+    verified.verified_account,
+    registrations.session_cnt
+FROM registrations
+LEFT JOIN revenue_usd 
+  ON registrations.continent=revenue_usd.continent
+LEFT JOIN verified 
+  ON registrations.continent=verified.continent
+ORDER BY revenue DESC;
+
+
